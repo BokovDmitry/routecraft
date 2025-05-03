adc8bc856f11e334b3a94aa86c8c0969f62fa4ce
Dmytro Bokov <dimabokov5@gmail.com>
2025-05-03 14:16:38 +0100

    databases created

---
+++ database/migrations/2025_05_03_124702_create_routes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('destination_city');
+            $table->integer('days');
+            $table->decimal('budget', 10, 2);
+            $table->decimal('rating', 3, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+};
